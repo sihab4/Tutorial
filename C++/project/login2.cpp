8747ad92c92e756b540114ad40d8c9a113f7c02f
sihab4 <rayzshyhab@gmail.com>
2022-04-28 16:04:59 +0700

    minor add

--- C++/project/login2.cpp
+++ C++/project/login2.cpp
@@ -6,12 +6,11 @@
 using namespace std;
 void login();
 void registr();
-void menu();
+void forgot();
 
 
 int main(){
     int pil;
-
     cout<<"WELCOME !!!"<<endl;
     cout<<endl;
 
@@ -33,7 +32,7 @@ int main(){
         break;
 
         case 3:
-        cout<<"Terimakasih sudah menggunakan aplikasi kami, kami akan kembali dengan fitur ini"<<endl;
+        forgot();
         break;
 
         case 4:
@@ -60,15 +59,16 @@ void login(){
     while(input>>u>>p){
             if(u==user && p==pass){
                     count=1;
-                    system("cls");
+                    
             }
     }
 
     input.close();
     if(count==1){
+            
             cout<<"Login Berhasil. Selamat datang !"<<endl;
-            cin.get();
             main();
+            
     }else{
             cout<<"Login Gagal, Cek Username dan Password kamu"<<endl;
     }
@@ -88,4 +88,98 @@ void registr(){
         system("cls");
         cout<<"Registrasi Berhasil"<<endl;
         main();
+}
+
+void forgot()
+{
+        int ch;
+        system("cls");
+        cout<<"1.Search your id by username"<<endl;
+        cout<<"2.Search your id by password"<<endl;
+        cout<<"3.Main menu"<<endl;
+        cout<<"Enter your choice :";
+        cin>>ch;
+        switch(ch)
+        {
+                case 1:
+                {
+                        int count=0;
+                        string searchuser,su,sp;
+                        cout<<"\nEnter your remembered username :";
+                        cin>>searchuser;
+                        
+                        ifstream searchu("database.txt");
+                        while(searchu>>su>>sp)
+                        {
+                                if(su==searchuser)
+                                {
+                                        count=1;
+                                }
+                        }
+                        searchu.close();
+                        if(count==1)
+                        {
+                                cout<<"\n\nHurray, account found\n";
+                                cout<<"\nYour password is "<<sp;
+                                cin.get();
+                                cin.get();
+                                system("cls");
+                                main();
+                        }
+                        else
+                        {
+                                cout<<"\nSorry, Your userID is not found in our database\n";
+                                cout<<"\nPlease kindly contact your system administrator for more details \n";
+                                cin.get();
+                                cin.get();
+                                main();
+                        }
+                        break;
+                }
+                case 2:
+                {
+                        int count=0;
+                        string searchpass,su2,sp2;
+                        cout<<"\nEnter the remembered password :";
+                        cin>>searchpass;
+                        
+                        ifstream searchp("database.txt");
+                        while(searchp>>su2>>sp2)
+                        {
+                                if(sp2==searchpass)
+                                {
+                                        count=1;
+                                }       
+                        }
+                        searchp.close();
+                        if(count==1)
+                        {
+                                cout<<"\nYour password is found in the database \n";
+                                cout<<"\nYour Id is : "<<su2;
+                                cin.get();
+                                cin.get();
+                                system("cls");
+                                main();
+                        }
+                        else
+                        {
+                                cout<<"Sorry, We cannot found your password in our database \n";
+                                cout<<"\nkindly contact your administrator for more information\n";
+                                cin.get();
+                                cin.get();
+                                main();
+                        }
+                
+                        break;
+                }
+                
+                case 3:
+                {
+                        cin.get();
+                        main();
+                }
+                default:
+                        cout<<"Sorry, You entered wrong choice. Kindly try again"<<endl;
+                        forgot();
+        }
 }
